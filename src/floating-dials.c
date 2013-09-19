f1f9578dc3862d465bb9bba895b52975244c4800
fugounashi <fugounashi+github@gmail.com>
2013-09-19 22:10:15 +0900

    release 1.0 added border around hour dial added screenshots icon and animation

--- src/floating-dials.c
+++ src/floating-dials.c
@@ -18,8 +18,6 @@ PBL_APP_INFO(MY_UUID, APP_NAME " " VERSION, "Koishi", MAJOR, MINOR,
 #define FOREGROUND GColorBlack
 #endif
 
-#define ROCKSHOT false
-//#define ROCKSHOT true
 #if ROCKSHOT
 #include "rockshot.h"
 #endif
@@ -28,9 +26,9 @@ Window window;
 
 Layer minute_dial, minute_hand, hour_dial, hour_hand, second_dial, second_hand;
 
-GPoint rotate_point(int angle, int radius, GPoint center) {
+GPoint rotate_point(int angle, int max, int radius, GPoint center) {
   GPoint point;
-  int32_t second_angle = TRIG_MAX_ANGLE * angle / 60;
+  int32_t second_angle = TRIG_MAX_ANGLE * angle / max;
   point.y = (-cos_lookup(second_angle) * radius / TRIG_MAX_RATIO) + center.y;
   point.x = (sin_lookup(second_angle) * radius / TRIG_MAX_RATIO) + center.x;
   return point;
@@ -43,29 +41,34 @@ GPoint rotate_point(int angle, int radius, GPoint center) {
 #define HOUR_RADIUS (MINUTE_RADIUS / 2) 
 #define THREE_MARK_LENGTH FIFTEEN_MARK_LENGTH
 #define ONE_MARK_LENGTH FIVE_MARK_LENGTH
-
+#define HOUR_BORDER 1
+#define HOUR_DIVISOR 2
+#define DIVISOR_MARK_LENGTH 0;
 
 void hour_dial_update(Layer *layer, GContext* gctx) {
   static char hour_text[] = "12";
   GPoint center = grect_center_point(&layer->frame);
   graphics_context_set_fill_color(gctx, BACKGROUND);
-  graphics_fill_circle(gctx, center, HOUR_RADIUS);
-  graphics_context_set_stroke_color(gctx, FOREGROUND);
   int32_t radius = HOUR_RADIUS ;
-  for (int angle=0; angle < 12; angle++){
+  graphics_fill_circle(gctx, center, radius);
+  radius -= HOUR_BORDER;
+  graphics_context_set_stroke_color(gctx, FOREGROUND);
+  for (int angle=0; angle < 12 * HOUR_DIVISOR; angle++){
     int32_t length;
-    if(angle % 3 == 0){
+    if(angle % (3 * HOUR_DIVISOR) == 0) {
       length = THREE_MARK_LENGTH;
-    } else {
+    } else if(angle % HOUR_DIVISOR == 0) {
       length = ONE_MARK_LENGTH;
+    } else {
+      length = DIVISOR_MARK_LENGTH;
     }
-    GPoint outer=rotate_point(angle * 5, radius, center);
-    GPoint inner=rotate_point(angle * 5, radius - length, center);
+    GPoint outer=rotate_point(angle, 12 * HOUR_DIVISOR, radius, center);
+    GPoint inner=rotate_point(angle, 12 * HOUR_DIVISOR, radius - length, center);
     graphics_draw_line(gctx, outer, inner);
 
-    if (angle % 3 == 0) {
-      GPoint text_center=rotate_point(angle * 5, radius - THREE_MARK_LENGTH - 10 , center);
-      snprintf(hour_text, sizeof(hour_text), "%2d", angle?angle:12);
+    if (angle % (3 * HOUR_DIVISOR) == 0) {
+      GPoint text_center=rotate_point(angle, 12 * HOUR_DIVISOR, radius - THREE_MARK_LENGTH - 10 , center);
+      snprintf(hour_text, sizeof(hour_text), "%2d", angle?angle / HOUR_DIVISOR:12);
       graphics_text_draw(gctx, hour_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
 			 GRect(text_center.x - 10, text_center.y - 9, 20, 14),
 			 GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);    
@@ -88,12 +91,12 @@ void minute_dial_update(Layer *layer, GContext* gctx) {
     } else{
       length=0;
     }
-    GPoint outer=rotate_point(angle, radius, center);
-    GPoint inner=rotate_point(angle, radius - length, center);
+    GPoint outer=rotate_point(angle, 60, radius, center);
+    GPoint inner=rotate_point(angle, 60, radius - length, center);
     graphics_draw_line (gctx, outer, inner);
 
     if(angle % 5 == 0) {
-      GPoint text_center=rotate_point(angle, radius - FIFTEEN_MARK_LENGTH - 10 , center);
+      GPoint text_center=rotate_point(angle, 60, radius - FIFTEEN_MARK_LENGTH - 10 , center);
       snprintf(minute_text, sizeof(minute_text), "%02d", angle);
       graphics_text_draw(gctx, minute_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
 			 GRect(text_center.x - 10, text_center.y - 9, 20, 14),
@@ -113,10 +116,10 @@ void minute_hand_update(Layer *layer, GContext* gctx) {
   get_time(&time);
 #ifdef TEST
   gpath_rotate_to(&minute_hand_path, TRIG_MAX_ANGLE * time.tm_sec / 60);
-  GPoint hour=rotate_point(time.tm_sec, HOUR_RADIUS, GPoint (0, 0));
+  GPoint hour=rotate_point(time.tm_sec, 60, HOUR_RADIUS, GPoint (0, 0));
 #else
   gpath_rotate_to(&minute_hand_path, TRIG_MAX_ANGLE * time.tm_min / 60);
-  GPoint hour=rotate_point(time.tm_min, HOUR_RADIUS, GPoint (0, 0));
+  GPoint hour=rotate_point(time.tm_min, 60, HOUR_RADIUS, GPoint (0, 0));
 #endif
   gpath_draw_filled(gctx, &minute_hand_path);
   gpath_draw_outline(gctx, &minute_hand_path);
