4a6b7d53549988c7f9288c3fabd46bc09b4b1a04
fugounashi <fugounashi+github@gmail.com>
2014-01-27 02:46:55 +0900

    converted to 2.0

--- src/floating-dials.c
+++ src/floating-dials.c
@@ -1,26 +1,24 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-#include "config.h" // from build-all
+static Window *window;
+static Layer *minute_dial, *minute_hand, *hour_dial, *hour_hand, *second_dial, *second_hand;
+
+#define TWENTYFOUR true
 
 #ifndef TWENTYFOUR
-#define MY_UUID { 0xA7, 0x13, 0xC7, 0x01, 0x7B, 0xB1, 0x4E, 0x19, 0x9B, 0xEF, 0x5A, 0xF2, 0x15, 0xD6, 0xFC, 0x51 }
-#define APP_NAME "Floating Dials"
+//#define MY_UUID { 0xA7, 0x13, 0xC7, 0x01, 0x7B, 0xB1, 0x4E, 0x19, 0x9B, 0xEF, 0x5A, 0xF2, 0x15, 0xD6, 0xFC, 0x51 }
+//#define APP_NAME "Floating Dials"
 #define MAX_HOURS 12
 #define MAX_HOURS_LABEL 12
 #define HOUR_DIVISOR 2
 #else
-#define MY_UUID { 0xA7, 0x13, 0xC7, 0x01, 0x7B, 0xB1, 0x4E, 0x19, 0x9B, 0xEF, 0x5A, 0xF2, 0x15, 0xD6, 0xFC, 0x52 }
-#define APP_NAME "Floating Dials (24h)"
+//#define MY_UUID { 0xA7, 0x13, 0xC7, 0x01, 0x7B, 0xB1, 0x4E, 0x19, 0x9B, 0xEF, 0x5A, 0xF2, 0x15, 0xD6, 0xFC, 0x52 }
+//#define APP_NAME "Floating Dials (24h)"
 #define MAX_HOURS 24
 #define MAX_HOURS_LABEL 0
 #define HOUR_DIVISOR 1
 #define HOUR_COUNTER 3
 #endif
-PBL_APP_INFO(MY_UUID, APP_NAME " " VERSION, "Koishi", MAJOR, MINOR, 
-             RESOURCE_ID_IMAGE_MENU_ICON, 
-             APP_INFO_WATCH_FACE);
 
 #ifndef INVERTED
 #define BACKGROUND GColorBlack
@@ -30,13 +28,6 @@ PBL_APP_INFO(MY_UUID, APP_NAME " " VERSION, "Koishi", MAJOR, MINOR,
 #define FOREGROUND GColorBlack
 #endif
 
-#if ROCKSHOT
-#include "rockshot.h"
-#endif
-
-Window window;
-
-Layer minute_dial, minute_hand, hour_dial, hour_hand, second_dial, second_hand;
 
 GPoint rotate_point(int angle, int max, int radius, GPoint center) {
   GPoint point;
@@ -59,7 +50,8 @@ GPoint rotate_point(int angle, int max, int radius, GPoint center) {
 
 void hour_dial_update(Layer *layer, GContext* gctx) {
   static char hour_text[] = "12";
-  GPoint center = grect_center_point(&layer->frame);
+  GRect frame=layer_get_frame(layer);
+  GPoint center = grect_center_point(&frame);
   graphics_context_set_fill_color(gctx, BACKGROUND);
   int32_t radius = HOUR_RADIUS ;
   graphics_fill_circle(gctx, center, radius);
@@ -87,7 +79,7 @@ void hour_dial_update(Layer *layer, GContext* gctx) {
     if (angle % (((MAX_HOURS / HOUR_MARKS)) * HOUR_DIVISOR) == 0) {
       GPoint text_center=rotate_point(angle, MAX_HOURS * HOUR_DIVISOR, radius - THREE_MARK_LENGTH - 10 , center);
       snprintf(hour_text, sizeof(hour_text), "%2d", angle?angle / HOUR_DIVISOR:MAX_HOURS_LABEL);
-      graphics_text_draw(gctx, hour_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
+      graphics_draw_text(gctx, hour_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
 			 GRect(text_center.x - 10, text_center.y - 9, 20, 14),
 			 GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);    
     }
@@ -99,7 +91,8 @@ void minute_dial_update(Layer *layer, GContext* gctx) {
   static char minute_text[] = "59";
   graphics_context_set_stroke_color(gctx, FOREGROUND);
   graphics_context_set_text_color(gctx, FOREGROUND);
-  GPoint center = grect_center_point(&layer->frame);
+  GRect frame=layer_get_frame(layer);
+  GPoint center = grect_center_point(&frame);
   for (int angle=0; angle < 60; angle++){
     int32_t radius = MINUTE_RADIUS ;
     int32_t length;
@@ -117,33 +110,34 @@ void minute_dial_update(Layer *layer, GContext* gctx) {
     if(angle % 5 == 0) {
       GPoint text_center=rotate_point(angle, 60, radius - FIFTEEN_MARK_LENGTH - 10 , center);
       snprintf(minute_text, sizeof(minute_text), "%02d", angle);
-      graphics_text_draw(gctx, minute_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
+      graphics_draw_text(gctx, minute_text, fonts_get_system_font(FONT_KEY_GOTHIC_14),
 			 GRect(text_center.x - 10, text_center.y - 9, 20, 14),
 			 GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);    
     }
   }
 }
 
-GPath minute_hand_path, hour_hand_path;
+static GPath *minute_hand_path, *hour_hand_path;
 
 void minute_hand_update(Layer *layer, GContext* gctx) {
   graphics_context_set_fill_color(gctx, FOREGROUND);
   graphics_context_set_stroke_color(gctx, BACKGROUND);
   //graphics_context_set_fill_color(gctx, BACKGROUND);
   //graphics_context_set_stroke_color(gctx, FOREGROUND);
-  PblTm time;
-  get_time(&time);
-#ifdef TEST
-  gpath_rotate_to(&minute_hand_path, TRIG_MAX_ANGLE * time.tm_sec / 60);
-  GPoint hour=rotate_point(time.tm_sec, 60, HOUR_RADIUS, GPoint (0, 0));
-#else
-  gpath_rotate_to(&minute_hand_path, TRIG_MAX_ANGLE * time.tm_min / 60);
-  GPoint hour=rotate_point(time.tm_min, 60, HOUR_RADIUS, GPoint (0, 0));
-#endif
-  gpath_draw_filled(gctx, &minute_hand_path);
-  gpath_draw_outline(gctx, &minute_hand_path);
 
-  layer_set_bounds(&hour_dial,GRect (-hour.x, -hour.y, 144, 168));	
+  time_t now = time(NULL);
+  struct tm *time = localtime(&now);
+#ifdef TEST
+  gpath_rotate_to(minute_hand_path, TRIG_MAX_ANGLE * time->tm_sec / 60);
+  GPoint hour=rotate_point(time->tm_sec, 60, HOUR_RADIUS, GPoint (0, 0));
+#else
+  gpath_rotate_to(minute_hand_path, TRIG_MAX_ANGLE * time->tm_min / 60);
+  GPoint hour=rotate_point(time->tm_min, 60, HOUR_RADIUS, GPoint (0, 0));
+#endif
+  gpath_draw_filled(gctx, minute_hand_path);
+  gpath_draw_outline(gctx, minute_hand_path);
+
+  layer_set_bounds(hour_dial,GRect (-hour.x, -hour.y, 144, 168));	
 
 }
 
@@ -152,16 +146,16 @@ void hour_hand_update(Layer *layer, GContext* gctx) {
   graphics_context_set_stroke_color(gctx, BACKGROUND);
   //graphics_context_set_fill_color(gctx, BACKGROUND);
   //graphics_context_set_stroke_color(gctx, FOREGROUND);
-  PblTm time;
-  get_time(&time);
+  time_t now = time(NULL);
+  struct tm *time = localtime(&now);
 #ifdef TEST
-  gpath_rotate_to(&hour_hand_path, TRIG_MAX_ANGLE * time.tm_sec / 60);
+  gpath_rotate_to(hour_hand_path, TRIG_MAX_ANGLE * time->tm_sec / 60);
 #else
-  //  gpath_rotate_to(&hour_hand_path, TRIG_MAX_ANGLE * ( (time.tm_hour + (time.tm_min / 60)) / 12 ) );
-  gpath_rotate_to(&hour_hand_path, (TRIG_MAX_ANGLE * (((time.tm_hour % MAX_HOURS) * 6) + (time.tm_min / 10))) / (MAX_HOURS * 6));
+  //  gpath_rotate_to(hour_hand_path, TRIG_MAX_ANGLE * ( (time->tm_hour + (time->tm_min / 60)) / 12 ) );
+  gpath_rotate_to(hour_hand_path, (TRIG_MAX_ANGLE * (((time->tm_hour % MAX_HOURS) * 6) + (time->tm_min / 10))) / (MAX_HOURS * 6));
 #endif
-  gpath_draw_filled(gctx, &hour_hand_path);
-  gpath_draw_outline(gctx, &hour_hand_path);
+  gpath_draw_filled(gctx, hour_hand_path);
+  gpath_draw_outline(gctx, hour_hand_path);
 }
 
 static const GPathInfo minute_hand_points = {
@@ -181,67 +175,75 @@ static const GPathInfo hour_hand_points = {
   }
 };
 
+static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(minute_hand);
+  layer_mark_dirty(hour_hand);
+}
+
+static void window_load(Window *window) {
+  // create all layers and gpaths
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
 #define my_layer_init(Layer)			\
-  layer_init(&Layer, window.layer.frame);	\
-  Layer.update_proc = &Layer ## _update;	\
-  layer_add_child(&window.layer, &Layer);
+  Layer=layer_create(bounds); \
+  layer_set_update_proc(Layer, Layer ## _update); \
+  layer_add_child(window_layer, Layer);
 
-
-void handle_init(AppContextRef ctx) {
-  window_init(&window, "Floating Dials");
-  window_stack_push(&window, true /* Animated */);
-
-  window_set_background_color(&window, BACKGROUND);
-  resource_init_current_app(&APP_RESOURCES);
-
-  gpath_init(&minute_hand_path, &minute_hand_points);
-  GPoint center = grect_center_point(&window.layer.bounds);
-  gpath_move_to(&minute_hand_path, center);
-
+  minute_hand_path=gpath_create(&minute_hand_points);
+  GPoint center = grect_center_point(&bounds);
+  gpath_move_to(minute_hand_path, center);
   my_layer_init(minute_dial);
   my_layer_init(minute_hand);
+
   my_layer_init(hour_dial);
 
-  gpath_init(&hour_hand_path, &hour_hand_points);
-  center = grect_center_point(&hour_dial.bounds);
-  gpath_move_to(&hour_hand_path, center);
+  hour_hand_path=gpath_create(&hour_hand_points);
+  bounds = layer_get_bounds(hour_dial);
+  center = grect_center_point(&bounds);
+  gpath_move_to(hour_hand_path, center);
 
-  layer_init(&hour_hand, hour_dial.frame);
-  hour_hand.update_proc = &hour_hand_update;
-  layer_add_child(&hour_dial, &hour_hand);
-
-#if ROCKSHOT
-  rockshot_init(ctx);
-#endif
+  GRect frame=layer_get_frame(hour_dial);
+  hour_hand=layer_create(frame);
+  layer_set_update_proc(hour_hand, hour_hand_update);
+  layer_add_child(hour_dial, hour_hand);
 }
 
-static void handle_tick(AppContextRef ctx, PebbleTickEvent* t) {
-  layer_mark_dirty(&minute_hand);
-  layer_mark_dirty(&hour_hand);
+static void window_unload(Window *window) {
+  // destroy all layers and gpaths
+  layer_destroy(minute_dial);
+  layer_destroy(minute_hand);
+  layer_destroy(hour_dial);
+  layer_destroy(hour_hand);
+  layer_destroy(second_dial);
+  layer_destroy(second_hand);
+  gpath_destroy(hour_hand_path);
+  gpath_destroy(minute_hand_path);
+
 }
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-#if ROCKSHOT
-    .messaging_info = {
-      .buffer_sizes = {
-        .inbound = 64, // inbound buffer size in bytes
-        .outbound = 256, // outbound buffer size in bytes
-      },
-    },
-#endif
-    .init_handler = &handle_init,
-    .tick_info = {
-      .tick_handler = &handle_tick,
-#ifdef TEST
-      .tick_units = SECOND_UNIT
-#else
-      .tick_units = MINUTE_UNIT
-#endif
-    }
-  };
-#if ROCKSHOT
-  rockshot_main(&handlers);
-#endif
-  app_event_loop(params, &handlers);
+static void init(void) {
+  // create window
+  window = window_create();
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  //Layer *root_layer = window_get_root_layer(window);
+  const bool animated = true;
+  window_stack_push(window, animated);
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+}
+
+static void deinit(void) {
+  // destroy window
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+  app_event_loop();
+  deinit();
+  return(0);
 }
