b434c5fc53e5f166eb6e427174c56f3021c3e904
fugounashi <fugounashi+github@gmail.com>
2014-01-27 14:04:32 +0900

    fixed icon release 2.0.0

--- appinfo.json.cpp
+++ appinfo.json.cpp
@@ -2,16 +2,14 @@
 
 #ifndef TWENTYFOUR
   #define UUID "a713c701-7bb1-4e19-9bef-5af215d6fc51"
-  #define APP_NAME "Floating Dials"
 #else
   #define UUID "a713c701-7bb1-4e19-9bef-5af215d6fc52"
-  #define APP_NAME "Floating Dials (24h)"
 #endif
 
 {
   "uuid": UUID,
-  "shortName": APP_NAME,
-  "longName": APP_NAME,
+  "shortName": SHORTNAME,
+  "longName": LONGNAME,
   "versionLabel": VERSION,
   "versionCode": VERSIONCODE,
   "companyName": "Koishi Horology",
@@ -22,6 +20,7 @@
   "resources": {
     "media": [
       {
+        "menuIcon": true,
         "type": "png",
         "name": "IMAGE_MENU_ICON",
         "file": "images/floating-dials-icon.png"
