e9613b2e08d517df0eba6250b75f99150c582ff6
fugounashi <fugounashi+github@gmail.com>
2014-03-27 22:17:19 +0900

    added date.  dynamic load/unload crashes pebble

--- src/floating-dials.c
+++ src/floating-dials.c
@@ -7,7 +7,7 @@ GColor BACKGROUND;
 GColor FOREGROUND;
 
 static Window *window;
-static Layer *minute_dial, *minute_hand, *hour_dial, *hour_hand, *second_dial, *second_hand;
+static Layer *minute_dial, *minute_hand, *hour_dial, *hour_hand, *second_dial, *second_hand, *date_dial, *date_hand;
 
 GPoint rotate_point(int angle, int max, int radius, GPoint center) {
   GPoint point;
@@ -30,6 +30,9 @@ GPoint rotate_point(int angle, int max, int radius, GPoint center) {
 
 #define SECOND_RADIUS 22
 #define SECOND_OFFSET 24
+
+bool wasseconds = false;
+bool wasdate = false;
 
 void hour_dial_update(Layer *layer, GContext* gctx) {
   int MAX_HOURS_LABEL;
@@ -113,6 +116,8 @@ void minute_dial_update(Layer *layer, GContext* gctx) {
 
 
 void second_dial_update(Layer *layer, GContext* gctx) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "second_dial_update");
+
   GRect frame=layer_get_frame(layer);
   GPoint center = grect_center_point(&frame);
   graphics_context_set_fill_color(gctx, BACKGROUND);
@@ -133,7 +138,30 @@ void second_dial_update(Layer *layer, GContext* gctx) {
 }
 
 
-static GPath *minute_hand_path, *hour_hand_path, *second_hand_path;
+void date_dial_update(Layer *layer, GContext* gctx) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "date_dial_update");
+
+  GRect frame=layer_get_frame(layer);
+  GPoint center = grect_center_point(&frame);
+  graphics_context_set_fill_color(gctx, BACKGROUND);
+  int32_t radius = SECOND_RADIUS ;
+  graphics_fill_circle(gctx, center, radius);
+  radius -= HOUR_BORDER;
+  graphics_context_set_stroke_color(gctx, FOREGROUND);
+  graphics_context_set_text_color(gctx, FOREGROUND);
+  for (int angle=0; angle < 31; angle += 2){
+    int32_t length = DIVISOR_MARK_LENGTH;
+    if(angle % 8 == 0) {
+      length = ONE_MARK_LENGTH;
+    } 
+    GPoint outer=rotate_point(angle, 32, radius, center);
+    GPoint inner=rotate_point(angle, 32, radius - length, center);
+    graphics_draw_line(gctx, outer, inner);
+  }
+}
+
+
+static GPath *minute_hand_path, *hour_hand_path;
 
 void minute_hand_update(Layer *layer, GContext* gctx) {
   graphics_context_set_fill_color(gctx, FOREGROUND);
@@ -147,6 +175,8 @@ void minute_hand_update(Layer *layer, GContext* gctx) {
   layer_set_bounds(hour_dial,GRect (-hour.x, -hour.y, 144, 168));	
   GPoint second=rotate_point((time->tm_min + 15) % 60, 60, SECOND_RADIUS + SECOND_OFFSET, GPoint (0, 0));
   layer_set_bounds(second_dial,GRect (second.x, second.y, 144, 168));	
+  GPoint date=rotate_point((time->tm_min - 15) % 60, 60, SECOND_RADIUS + SECOND_OFFSET, GPoint (0, 0));
+  layer_set_bounds(date_dial,GRect (date.x, date.y, 144, 168));	
 }
 
 void hour_hand_update(Layer *layer, GContext* gctx) {
@@ -165,6 +195,8 @@ void hour_hand_update(Layer *layer, GContext* gctx) {
 }
 
 void second_hand_update(Layer *layer, GContext* gctx) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "second_hand_update");
+
   graphics_context_set_fill_color(gctx, FOREGROUND);
   time_t now = time(NULL);
   struct tm *time = localtime(&now);
@@ -179,6 +211,20 @@ void second_hand_update(Layer *layer, GContext* gctx) {
   graphics_context_set_stroke_color(gctx, FOREGROUND);
   graphics_draw_line(gctx, outer, center);
 }
+
+
+void date_hand_update(Layer *layer, GContext* gctx) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "date_hand_update");
+  graphics_context_set_fill_color(gctx, FOREGROUND);
+  time_t now = time(NULL);
+  struct tm *time = localtime(&now);
+  GRect frame=layer_get_frame(layer);
+  GPoint center = grect_center_point(&frame);
+  GPoint outer=rotate_point(time->tm_mday, 32, (SECOND_RADIUS - THREE_MARK_LENGTH), center);
+  graphics_context_set_stroke_color(gctx, FOREGROUND);
+  graphics_draw_line(gctx, outer, center);
+}
+
 
 static const GPathInfo minute_hand_points = {
   3,
@@ -196,21 +242,30 @@ static const GPathInfo hour_hand_points = {
     { 0, (THREE_MARK_LENGTH - HOUR_RADIUS) }
   }
 };
-static const GPathInfo second_hand_points = {
-  3,
-  (GPoint []) {
-    { -(THREE_MARK_LENGTH - SECOND_RADIUS)/4, 0 },
-    { (THREE_MARK_LENGTH - SECOND_RADIUS)/4, 0 },
-    { 0, (THREE_MARK_LENGTH - SECOND_RADIUS) }
-  }
-};
+
 
 static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(minute_hand);
-  layer_mark_dirty(hour_hand);
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "handle_tick");
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "handle_tick getSeconds:%d wasseconds:%d", getSeconds(), wasseconds);
+  if (wasseconds && (units_changed & SECOND_UNIT)) {
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "handle_tick layer_mark_dirty(second_hand)");
+    layer_mark_dirty(second_hand);
+  }
+  if (units_changed & MINUTE_UNIT) {
+    layer_mark_dirty(minute_hand);
+    if (units_changed & HOUR_UNIT) {
+      layer_mark_dirty(hour_hand);
+      if (wasdate && (units_changed & DAY_UNIT)) {
+	layer_mark_dirty(date_hand);
+      }
+    }
+  }
 }
 
+
 static void window_load(Window *window) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window_load");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window_load start getSeconds:%d wasseconds:%d", getSeconds(), wasseconds);
 
   if(getBackground()){
     BACKGROUND=GColorWhite;
@@ -254,46 +309,71 @@ static void window_load(Window *window) {
   layer_add_child(hour_dial, hour_hand);
 
   my_layer_init(second_dial);
-
-  second_hand_path=gpath_create(&second_hand_points);
   bounds = layer_get_bounds(second_dial);
-  center = grect_center_point(&bounds);
-  gpath_move_to(second_hand_path, center);
-
   frame=layer_get_frame(second_dial);
   second_hand=layer_create(frame);
   layer_set_update_proc(second_hand, second_hand_update);
   layer_add_child(second_dial, second_hand);
 
+  my_layer_init(date_dial);
+  bounds = layer_get_bounds(date_dial);
+  frame=layer_get_frame(date_dial);
+  date_hand=layer_create(frame);
+  layer_set_update_proc(date_hand, date_hand_update);
+  layer_add_child(date_dial, date_hand);
+
+  if(getSeconds()){
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "window_load seconds true");
+    wasseconds = true;
+    tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
+  } else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "window_load seconds false");
+    wasseconds = false;
+    layer_set_hidden((Layer *)second_dial, true);
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+  } 
+  if(getDate()){
+    wasdate = true;
+  } else {
+    wasdate = false;
+    layer_set_hidden((Layer *)date_dial, true);
+  } 
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window_load end getSeconds:%d wasseconds:%d", getSeconds(), wasseconds);
+
 }
 
 static void window_unload(Window *window) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "window_unload getSeconds:%d wasseconds:%d", getSeconds(), wasseconds);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "window_unload");
   // destroy all layers and gpaths
+  tick_timer_service_unsubscribe();
   layer_destroy(minute_dial);
   layer_destroy(minute_hand);
+  gpath_destroy(minute_hand_path);
   layer_destroy(hour_dial);
   layer_destroy(hour_hand);
+  gpath_destroy(hour_hand_path);
   layer_destroy(second_dial);
   layer_destroy(second_hand);
-  gpath_destroy(hour_hand_path);
-  gpath_destroy(minute_hand_path);
-  gpath_destroy(second_hand_path);
+  layer_destroy(date_dial);
+  layer_destroy(date_hand);
 }
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
   autoconfig_in_received_handler(iter, context);
   //APP_LOG(APP_LOG_LEVEL_DEBUG, "in_received_handler background:%d seconds:%d hourdialtype:%d handwidth:%d", getBackground(), getSeconds(), getHourdialtype(), (int)getHandwidth());
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "in_received_handler background:%d hourdialtype:%d ", getBackground(), getHourdialtype());
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "in_received_handler background:%d hourdialtype:%d seconds:%d", getBackground(), getHourdialtype(), getSeconds());
 
   //update display
-  window_unload(window);
-  window_load(window);
+  // crashfest:
+  //window_unload(window);
+  //window_load(window);
 }
 
 static void init(void) {
   autoconfig_init();
   //APP_LOG(APP_LOG_LEVEL_DEBUG, "init background:%d seconds:%d hourdialtype:%d handwidth:%d", getBackground(), getSeconds(), getHourdialtype(), (int)getHandwidth());
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "init background:%d hourdialtype:%d", getBackground(), getHourdialtype());
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "init background:%d hourdialtype:%d seconds:%d", getBackground(), getHourdialtype(), getSeconds());
 
   // create window
   window = window_create();
@@ -303,13 +383,12 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-  tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
-
   app_message_register_inbox_received(in_received_handler);
 }
 
 static void deinit(void) {
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "deinit getSeconds:%d wasseconds:%d", getSeconds(), wasseconds);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "deinit background:%d hourdialtype:%d seconds:%d", getBackground(), getHourdialtype(), getSeconds());
   autoconfig_deinit();
   window_destroy(window);
 }
